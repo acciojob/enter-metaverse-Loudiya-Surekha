07ecd9334ea634dc97ece992c98d47fc6d78c159
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-21 00:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+    let btn=document.getElementById("enterBtn");
+    let statusElem=document.getElementById("status");
+
+	btn.addEventListener('click', function(){
+	statusElem.innerHTML="<h1>Entered Metaverse</h1>"
+		
+	});
+	
+});
